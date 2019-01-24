7814bac937915a72b5b91aafceb146f75d1f636b
Scott Ian <sim2487@gmail.com>
2019-01-24 08:37:13 -0800

    mergeSort

--- mergesort.js
+++ mergesort.js
@@ -1,41 +1,45 @@
 let dataset =
-  '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5'
-dataset = dataset.split(' ')
-// console.log(dataset)
+  '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5';
+dataset = dataset.split(' ');
+dataset.forEach((str, i) => {
+  dataset[i] = parseInt(str);
+});
+
+console.log(dataset);
 
 function mergeSort(array) {
   if (array.length <= 1) {
-    return array
+    return array;
   }
 
-  const middle = Math.floor(array.length / 2)
-  let left = array.slice(0, middle)
-  let right = array.slice(middle, array.length)
+  const middle = Math.floor(array.length / 2);
+  let left = array.slice(0, middle);
+  let right = array.slice(middle, array.length);
 
-  left = mergeSort(left)
-  right = mergeSort(right)
-  return merge(left, right, array)
+  left = mergeSort(left);
+  right = mergeSort(right);
+  return merge(left, right, array);
 }
 function merge(left, right, array) {
-  let leftIndex = 0
-  let rightIndex = 0
-  let outputIndex = 0
+  let leftIndex = 0;
+  let rightIndex = 0;
+  let outputIndex = 0;
   while (leftIndex < left.length && rightIndex < right.length) {
     if (left[leftIndex] < right[rightIndex]) {
-      array[outputIndex++] = left[leftIndex++]
+      array[outputIndex++] = left[leftIndex++];
     } else {
-      array[outputIndex++] = right[rightIndex++]
+      array[outputIndex++] = right[rightIndex++];
     }
   }
 
   for (let i = leftIndex; i < left.length; i++) {
-    array[outputIndex++] = left[i]
+    array[outputIndex++] = left[i];
   }
 
   for (let i = rightIndex; i < right.length; i++) {
-    array[outputIndex++] = right[i]
+    array[outputIndex++] = right[i];
   }
-  return array
+  return array;
 }
 
-console.log(mergeSort(dataset))
+console.log(mergeSort(dataset));
